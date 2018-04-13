b4caf93e283933a07a264e05df6dbb85d9f84828
Kazuki Kasahara <nuderebatsin.roal@gmail.com>
2018-04-13 22:59:11 +0900

    Stop requesting when authenticate error occuerd during fetching league ranking

--- league_ranking/get_league_ranking.py
+++ league_ranking/get_league_ranking.py
@@ -39,6 +39,9 @@ def _get_splatoon_ranking(match_date_uri):
     r = requests.get(
         SPLATOON2_LEAGUE_MATCH_RANKING_URI.format(match_date_uri),
         cookies=COOKIES)
+    if r.status_code == 403:
+        print("error: authentication Error. make sure your iksm_session.")
+        sys.exit(1)
     return r.text
 
 
